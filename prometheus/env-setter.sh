56ea5675fa71b11e1acd1f57d33ee6c082b8f757
aleskandro <aleskandro@lucylaika.ovh>
2018-07-19 15:52:40 +0100

    Prometheus workaround for targets on swarm with "VERY" dynamic ip

--- prometheus/env-setter.sh
+++ prometheus/env-setter.sh
@@ -2,12 +2,14 @@
 # - targets: ['raphtory_partitionManager_1:19200', 'raphtory_router_1:19300', 'raphtory_updater_1:19400']
 
 TARGETS="        - targets: ["
-PREFIX=10.0.0
+PREFIX=`ip addr show eth1 | tail -n 2 | head -n 1 | sed "s/.*inet \([0-9]\+\..*\)\.[0-9]\+\/[0-9]\+.*/\1/"`
 PORT=11600
 
-for i in `seq 0 254`; do
+for i in `seq 0 253`; do
     TARGETS="${TARGETS}'${PREFIX}.${i}:${PORT}', "
 done
+TARGETS="${TARGETS}'${PREFIX}.254:${PORT}']"
+echo $TARGETS
+echo "${TARGETS}" >> /etc/prometheus/prometheus.yml
 
-echo ${TARGETS}
-
+/bin/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/etc/prometheus/console_libraries --web.console.templates=/etc/prometheus/consoles
