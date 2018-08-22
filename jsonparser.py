b7b122bede2ea319d929a55575eca979df3549c7
miratepuffin <piratemuffin@hotmail.co.uk>
2018-08-22 03:46:03 +0100

    Pointed json parser to moe

--- jsonparser.py
+++ jsonparser.py
@@ -2,10 +2,10 @@
 import json
 import urllib2
 
-response = urllib2.urlopen('http://moon15:8888/api/v1/query?query=akka_system_dead_letters_total')
+response = urllib2.urlopen('http://moe.eecs.qmul.ac.uk:8888/api/v1/query?query=akka_system_dead_letters_total')
 parsed = json.load(response)["data"]["result"]
 sum = 0
-for metric in parsed: 
+for metric in parsed:
     sum += int(metric["value"][1])
 
 if (sum > 500):
