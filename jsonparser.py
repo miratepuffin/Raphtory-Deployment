9d0ede4715c929adfd3fe2b81cd2bc537be088a3
miratepuffin <piratemuffin@hotmail.co.uk>
2018-08-23 02:20:39 +0100

    Final fix for jsonparser

--- jsonparser.py
+++ jsonparser.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 import json
 import urllib2
+import time
 
-response = urllib2.urlopen('http://moe.eecs.qmul.ac.uk:8888/api/v1/query?query=akka_system_dead_letters_total')
-parsed = json.load(response)["data"]["result"]
 
 sum = 0
-while (sum > 500)
+while (sum < 500):
     time.sleep(10)
+    response = urllib2.urlopen('http://moe.eecs.qmul.ac.uk:8888/api/v1/query?query=akka_system_dead_letters_total')
+    parsed = json.load(response)["data"]["result"] 
     sum = 0
     for metric in parsed:
         sum += int(metric["value"][1])
